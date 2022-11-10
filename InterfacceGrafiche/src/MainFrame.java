9dcddc701420f9a78d1dd5e2453ca5512c86b468
Ricky0178 <riccardo.zucchelli@gmail.com>
2022-11-10 11:14:37 +0100

    first commit

---
+++ InterfacceGrafiche/src/MainFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+import frame.MyFrame;
+
+import java.awt.*;
+
+public class MainFrame {
+
+	public static void main(String[] args) {
+		MyFrame frm = new MyFrame();
+		
+				
+		
+		
+	}
+
+}
